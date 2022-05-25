22ef42bb8c27d50edf4d4b5fdcceed490ea169da
DSiminski <siminskidayane@gmail.com>
2022-05-24 21:08:58 -0300

    HistoricoRepository

--- src/main/java/br/com/consultorio/repository/HistoricoRepository.java
+++ src/main/java/br/com/consultorio/repository/HistoricoRepository.java
@@ -2,9 +2,21 @@ package br.com.consultorio.repository;
 
 import br.com.consultorio.entity.Historico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.time.LocalDateTime;
 
+@Repository
 public interface HistoricoRepository extends JpaRepository<Historico, Long> {
+
+    @Modifying
+    @Query("UPDATE Historico historico " +
+            "SET historico.excluido = :dataExcluido " +
+            "WHERE historico.id = :idHistorico")
+    public void updateStatus(
+            @Param("dataExcuido") LocalDateTime dataExcuido,
+            @Param("historico") Long idHistorico);
 }
